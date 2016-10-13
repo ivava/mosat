b08607e60638c71285d7007342828d17d038c936
galinovivan <9225913@gmail.com>
2016-10-14 00:36:39 +0300

    user

---
+++ backend/classes/User.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 12.10.16
+ * Time: 0:22
+ */
+class User
+{
+public $id;
+public $login;
+public $email;
+public $fullName;
+
+
+    public function __construct($fullName, $login, $mail)
+    {
+        $this->fullName = $fullName;
+        $this->login = $login;
+        $this->mail = $mail;
+    }
+
+
+
+    public function insert() {
+        if (!is_null($this->id)) {
+            trigger_error("User::insert(): file exxist", ERROR_DB);
+        } else {
+            $connect = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            $sql = "INSERT INTO usertbl (full_name, login, email) VALUES (:full_name, :login, :email)";
+            $st = $connect->prepare($sql);
+            $st->bindValue(":full_name", $this->fullName, PDO::PARAM_STR);
+            $st->bindValue(":login", $this->login, PDO::PARAM_STR);
+            $st->bindValue(":email", $this->email, PDO::PARAM_STR);
+            $st->execute();
+            $this->id = $connect->lastInsertId();
+            $connect = null;
+        }
+    }
+
+}
